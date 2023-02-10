9cb08773b4b6cc41be643640c72cff45d3abc162
himanshu808 <himanshushah.alt@gmail.com>
2023-02-09 21:01:42 -0800

    highlighting branch instrs in ir viz

--- ir_viz_tool/ir_viz.py
+++ ir_viz_tool/ir_viz.py
@@ -5,10 +5,13 @@ from app.parser.cfg import CFG
 from app.parser.basic_blocks import BB
 from app.custom_types import InstrNodeActual
 from app.tokenizer import Tokenizer
+from app.tokens import RELOP_TOKEN_OPCODE, OpCodeEnum
 
 
 colors = ["lightcoral", "grey67", "royalblue4"]
 c = 0
+
+BR_INSTRS = set(RELOP_TOKEN_OPCODE.values()).union({OpCodeEnum.BRA.value})
 
 
 class IRViz:
@@ -34,11 +37,24 @@ class IRViz:
 
     def add_pred_edge(self, bb: BB) -> None:
         global c
+
+        bb_first_instr_num: int = bb.get_first_instr_num()
         pred_bb: list[int] = self.cfg.get_predecessors(bb.bb_num)
         if not pred_bb:
             return
         for pred in pred_bb:
-            self.graph.edge(f"BB{pred}:s", f"BB{bb.bb_num}:n", color=colors[c % 3])
+            instr: InstrNodeActual = self.cfg.get_instr(self.cfg.get_bb_from_bb_num(pred).get_last_instr_num())
+            instr_num: int = -1
+            if instr.opcode in BR_INSTRS:
+                if instr.opcode == OpCodeEnum.BRA.value:
+                    instr_num = instr.left
+                else:
+                    instr_num = instr.right
+            if instr_num == bb_first_instr_num:
+                self.graph.edge(f"BB{pred}:s", f"BB{bb.bb_num}:n", color=colors[c % 3], label="br", style="bold")
+            else:
+                self.graph.edge(f"BB{pred}:s", f"BB{bb.bb_num}:n", color=colors[c % 3])
+
             c += 1
 
     def fmt_var_instr_map(self, bb: BB) -> str:
